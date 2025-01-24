f3336a6b2c39828437f78f4d94116253b2b7ec90
Mohammed Farooque Shaikh <41079646+farooque1@users.noreply.github.com>
2025-01-24 18:05:37 +0530

    Update Service.js

--- src/app/Component/Service.js
+++ src/app/Component/Service.js
@@ -1,77 +1,88 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Import AOS styles
 
 function Service() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Animation duration in milliseconds
+      easing: 'ease-in-out', // Easing for animations
+      once: true, // Whether animation should happen only once
+    });
+  }, []);
+
   return (
     <div>
-      <h1 className='text-white text-center text-4xl font-extrabold py-7'>Service</h1>
+      <h1 className="text-white text-center text-4xl font-extrabold py-7">Service</h1>
 
-      <div class="container">
-        <div class="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-blue-50">
-
-          <div class="flex flex-row-reverse md:contents">
-            <div class="bg-blue-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
-              <h3 class="font-semibold text-lg mb-1">Web Application Development</h3>
+      <div className="container">
+        <div className="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-blue-50">
+          <div
+            className="flex flex-row-reverse md:contents"
+            data-aos="fade-left" // Add AOS animation
+          >
+            <div className="bg-blue-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
+              <h3 className="font-semibold text-lg mb-1">Web Application Development</h3>
             </div>
-            <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-              <div class="h-full w-6 flex items-center justify-center">
-                <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
+            <div className="col-start-5 col-end-6 md:mx-auto relative mr-10">
+              <div className="h-full w-6 flex items-center justify-center">
+                <div className="h-full w-1 bg-blue-800 pointer-events-none"></div>
               </div>
-              <div class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"></div>
+              <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"></div>
             </div>
           </div>
 
-          <div class="flex md:contents">
-            <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
-              <div class="h-full w-6 flex items-center justify-center">
-                <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
+          <div className="flex md:contents" data-aos="fade-right">
+            <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+              <div className="h-full w-6 flex items-center justify-center">
+                <div className="h-full w-1 bg-blue-800 pointer-events-none"></div>
               </div>
-              <div class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"></div>
+              <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"></div>
             </div>
-            <div class="bg-blue-500 col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md">
-              <h3 class="font-semibold text-lg mb-1">User Interface (UI) Design</h3>
-            </div>
-          </div>
-          <div class="flex flex-row-reverse md:contents">
-            <div class="bg-blue-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
-              <h3 class="font-semibold text-lg mb-1">Performance Optimization</h3>
-            </div>
-            <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-              <div class="h-full w-6 flex items-center justify-center">
-                <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
-              </div>
-              <div class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"></div>
-            </div>
-          </div>
-          <div class="flex md:contents">
-            <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-              <div class="h-full w-6 flex items-center justify-center">
-                <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
-              </div>
-              <div class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"></div>
-            </div>
-            <div class="bg-blue-500 col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md">
-              <h3 class="font-semibold text-lg mb-1">Collaboration and Maintenance</h3>
+            <div className="bg-blue-500 col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md">
+              <h3 className="font-semibold text-lg mb-1">User Interface (UI) Design</h3>
             </div>
           </div>
 
-          <div class="flex flex-row-reverse md:contents">
-            <div class="bg-blue-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
-              <h3 class="font-semibold text-lg mb-1">Deployment and Hosting</h3>
+          <div className="flex flex-row-reverse md:contents" data-aos="fade-left">
+            <div className="bg-blue-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
+              <h3 className="font-semibold text-lg mb-1">Performance Optimization</h3>
             </div>
-            <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
-              <div class="h-full w-6 flex items-center justify-center">
-                <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
+            <div className="col-start-5 col-end-6 md:mx-auto relative mr-10">
+              <div className="h-full w-6 flex items-center justify-center">
+                <div className="h-full w-1 bg-blue-800 pointer-events-none"></div>
               </div>
-              <div class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"></div>
+              <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"></div>
             </div>
           </div>
-          
 
+          <div className="flex md:contents" data-aos="fade-right">
+            <div className="col-start-5 col-end-6 md:mx-auto relative mr-10">
+              <div className="h-full w-6 flex items-center justify-center">
+                <div className="h-full w-1 bg-blue-800 pointer-events-none"></div>
+              </div>
+              <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"></div>
+            </div>
+            <div className="bg-blue-500 col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md">
+              <h3 className="font-semibold text-lg mb-1">Collaboration and Maintenance</h3>
+            </div>
+          </div>
+
+          <div className="flex flex-row-reverse md:contents" data-aos="fade-left">
+            <div className="bg-blue-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
+              <h3 className="font-semibold text-lg mb-1">Deployment and Hosting</h3>
+            </div>
+            <div className="col-start-5 col-end-6 md:mx-auto relative mr-10">
+              <div className="h-full w-6 flex items-center justify-center">
+                <div className="h-full w-1 bg-blue-800 pointer-events-none"></div>
+              </div>
+              <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-blue-500 shadow"></div>
+            </div>
+          </div>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Service
+export default Service;
